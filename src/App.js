2204974593b7fac153f3b7814290d23598cb323d
Rob L <viewpl@gmail.com>
2019-03-27 12:44:59 +1100

    add search component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Search from './Search';
+
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <div className="container">
+          <Search />
+        </div>
+      </div>
+    );
   }
 }
 
---
+++ src/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <input
+        class="input"
+        type="text"
+        placeholder="Type anything to search..."
+      />
+    );
+  }
+}
+
+export default Search;
