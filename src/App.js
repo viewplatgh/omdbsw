aec919decb5e575091171f8dc5841de7886264a4
Rob L <viewpl@gmail.com>
2019-03-27 17:16:05 +1100

    move search to containers and add title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Search from './Search';
+import Search from './containers/Search';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="container">
+          <h1 className="title">OMDB api demo made by Rob Lao</h1>
           <Search />
         </div>
       </div>
--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { searchOMDB } from './actions';
+import { searchOMDB } from '../actions';
 
 class Search extends Component {
   keywordRef;
